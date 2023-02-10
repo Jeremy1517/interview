9d519f22f5ccf8a585f1d31bb0899ead16f4a464
Draden Liang Han Sheng <hanshengliang@outlook.com>
2023-02-10 11:02:37 +0800

    Enhance BE logic

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port = 3000
+const port = 9898
 
 app.get('/health', (req, res) => {
   res.send('Hello World!')
@@ -13,8 +13,20 @@ app.get('/services/check-rating-engine', (req, res) => {
   return res.end();
 })
 
+app.get('/services/check-rating-engine/details', (req, res) => {
+  res.jsonp({"usage": Math.random() })
+  res.status(200);
+  return res.end();
+})
+
 app.get('/services/check-insurance', (req, res) => {
   res.jsonp({"result": Math.random() < 0.65 ? "true" : "false"})
+  res.status(200);
+  return res.end();
+})
+
+app.get('/services/check-insurance/details', (req, res) => {
+  res.jsonp({"usage": Math.random() })
   res.status(200);
   return res.end();
 })
@@ -25,12 +37,24 @@ app.get('/services/check-auth', (req, res) => {
   return res.end();
 })
 
+app.get('/services/check-auth/details', (req, res) => {
+  res.jsonp({"usage": Math.random() })
+  res.status(200);
+  return res.end();
+})
+
 app.get('/services/check-policy', (req, res) => {
   res.jsonp({"result": Math.random() < 0.65 ? "true" : "false"})
   res.status(200);
   return res.end();
 })
 
+app.get('/services/check-policy/details', (req, res) => {
+  res.jsonp({"usage": Math.random() })
+  res.status(200);
+  return res.end();
+})
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`BE app listening on port ${port}`)
 })
